7ee457c721c9655d419320bb2559262378351c7e
stoneYuan3 <jackyuan110@gmail.com>
2023-04-03 12:29:09 -0700

    drag and drop function mostly complete, API connection still unstable 112-04-03

--- app/src/main/java/com/example/iat359_a3/MainActivity.java
+++ app/src/main/java/com/example/iat359_a3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.iat359_a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -54,19 +56,101 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         mutip10 = (TextView) findViewById(R.id.mutip10);
 
         results = new ArrayList();
+
+        //set touch listeners
+        item1.setOnTouchListener(this);
+        item2.setOnTouchListener(this);
+        item3.setOnTouchListener(this);
+        item4.setOnTouchListener(this);
+
+        //set drag listeners
+        mutip2.setOnDragListener(this);
+        mutip3.setOnDragListener(this);
+        mutip5.setOnDragListener(this);
+        mutip10.setOnDragListener(this);
+
     }
 
-    @Override
-    public boolean onDrag(View view, DragEvent dragEvent) {
-        return false;
+    private boolean identifyDrop(TextView view,int rootNum){
+        boolean result = false;
+        try{
+            int value = Integer.valueOf((String) view.getText());
+            Log.d("dragdrop","mutip" + rootNum + " is dropped upon, input is: "+value);
+            if(value % rootNum == 0 ){
+                result=true;
+            }
+        }
+        catch (Exception e){
+            result = false;
+        }
+        Log.d("dragdrop","mutip" + rootNum + " is dropped upon, result is: "+result);
+        return result;
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        return false;
+        TextView touchedItem = (TextView) view;
+        if (touchedItem.getText()!="" && motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            //the user has touched the View to drag it
+            //prepare the drag
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            //start dragging the item touched
+            view.startDrag(data, shadowBuilder, view, 0);
+//            Toast.makeText(this, "onTouch - startDrag", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
+    @Override
+    public boolean onDrag(View v, DragEvent dragEvent) {
 
+        switch (dragEvent.getAction()) {
+            case DragEvent.ACTION_DROP:
+                //handle the dragged view being dropped over a target view
+                View view = (View) dragEvent.getLocalState();
+                //view dragged item is being dropped on
+                TextView dropTarget = (TextView) v;
+                boolean droppable = false;
+
+                if(dropTarget.getId()==mutip2.getId()){
+                    droppable=identifyDrop(mutip2,2);
+                }
+                else if (dropTarget.getId()==mutip3.getId()){
+                    droppable=identifyDrop(mutip3,3);
+                }
+                else if (dropTarget.getId()==mutip5.getId()){
+                    droppable=identifyDrop(mutip5,5);
+                }
+                else if (dropTarget.getId()==mutip10.getId()){
+                    droppable=identifyDrop(mutip10,10);
+                }
+                else{
+                    Log.d("dragdrop","doesnt belong to any existing dropbox");
+                }
+
+                if(droppable){
+                    //view being dragged and dropped
+                    TextView dropped = (TextView) view;
+                    //update the text in the target view to reflect the data being dropped
+                    dropTarget.setText(""+ dropTarget.getText()+"\n" +dropped.getText());
+                    dropped.setText("");
+                    //make it bold to highlight the fact that an item has been dropped
+                    dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
+                }
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+                //no action necessary
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+    
     //check if device is connected to wifi at the start of the app
     public void checkConnection(){
         ConnectivityManager connectMgr =
@@ -76,7 +160,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             //fetch data
 
             String networkType = networkInfo.getTypeName().toString();
-            Toast.makeText(this, "connected to " + networkType, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "connected to " + networkType, Toast.LENGTH_SHORT).show();
         }
         else {
             //display error
@@ -88,7 +172,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private JSONObject jsonObj;
     public void buttonGetData(View view) {
         Log.d("buttonGetData", "running");
-
         url = "https://qrng.anu.edu.au/API/jsonI.php?length=4&type=uint8";
         Thread myThread = new Thread(new GetData());
         myThread.start();
@@ -115,12 +198,18 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 exception = e;
             }
             try{
-//                JSONObject jsonObj =new JSONObject(result);
                 jsonObj = new JSONObject(result);
                 Log.d("run", jsonObj.toString());
 
                 JSONArray numarr = jsonObj.getJSONArray("data");
                 Log.d("run", numarr.toString());
+
+                for (int i=0;i<numarr.length();i++){
+                    int element = numarr.getInt(i);
+                    String dataEach = String.valueOf(element);
+                    Log.d("result "+i, dataEach);
+                    results.add(element);
+                }
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -130,20 +219,16 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                         item2.setText(String.valueOf(results.get(1)));
                         item3.setText(String.valueOf(results.get(2)));
                         item4.setText(String.valueOf(results.get(3)));
+                        results.clear();
+                        result=null;
+                        jsonObj=null;
                     }
                 });
-
-                for (int i=0;i<numarr.length();i++){
-                    int element = numarr.getInt(i);
-                    String dataEach = String.valueOf(element);
-                    Log.d("result "+i, dataEach);
-                    results.add(element);
-                }
 
             }
             catch (Exception e){
                 Log.d("ReadWeatherJSON", e.getLocalizedMessage());
-                if(jsonObj == null){
+                if(jsonObj == null || result==null){
                     Log.d("APIresult", "API returns null, wait for 1 minute");
                     runOnUiThread(new Runnable() {
                         @Override
@@ -173,6 +258,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             conn.setConnectTimeout(15000 /* milliseconds */);
             conn.setRequestMethod("GET");
             conn.setDoInput(true);
+
+            conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)");
             // Starts the query
             conn.connect();
 
@@ -190,12 +277,16 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
         }catch (Exception e){
+            Log.e("readJSONData", "Error connecting to " + myurl, e);
             throw e;
         }
         finally {
             if (is != null) {
                 is.close();
+                Log.d("readJSONData", "is is null, closing");
+
             }
+            Log.d("readJSONData", "closing");
             conn.disconnect();
         }
     }
